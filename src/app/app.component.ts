5ea06106e3ca6804ec773b6f9174a00ec476bf72
Devin Mounts and Matt Smith <matt@>
2018-08-13 14:07:12 -0700

    add ngFor loop to create divs for each recipe.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from './models/recipe.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Recipe Box';
+
+  recipies: Recipe[] = [
+    new Recipe('Flan', ['Condensed Milk', 'Sugar', 'Cinnamon'], ['Mix Milk, sugar, and cinnamon', 'Bake at 400 for 15 min', 'Eat']),
+    new Recipe('Boiling Water', ['cold water'], ['add heat']),
+    new Recipe('Peanut Butter and Jealous', ['Bread', 'Peanut Butter', 'Jealousy'], ['Apply Peanut Butter and Jealousy to Bread', 'taunt your friends with your awesome sandwich', 'Eat'])
+  ];
+
 }
